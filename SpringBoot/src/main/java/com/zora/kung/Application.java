d330eb2e3ba264cac616914a1cac5d1cb277efb0
ZoraKung <835376597@qq.com>
2017-04-28 10:33:49 +0800

    init

---
+++ SpringBoot/src/main/java/com/zora/kung/Application.java
@@ -0,0 +1,14 @@
+package com.zora.kung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Zora on 2017/4/28.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
